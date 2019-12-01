08ffd63a2244409504ff7dcba4c19286d4728e6e
xjz <858959396@qq.com>
2019-12-01 13:22:32 +0800

    新增一个测试类

---
+++ src/test/Test02.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.sql.SQLOutput;
+
+/**
+ * className:Test02
+ * package:test
+ * Description:简单描述
+ *
+ * @data:2019/12/1 0001 13:22
+ * @author:xx
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
