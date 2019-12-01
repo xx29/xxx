eca9b66b42d7f7cb434f89d43e440b02f7289f3c
xjz <858959396@qq.com>
2019-12-01 12:10:16 +0800

    第一次提交

---
+++ src/test/Test01.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * className:Test01
+ * package:test
+ * Description:简单描述
+ *
+ * @data:2019/12/1 0001 12:06
+ * @author:xx
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("添加第一句话");
+    }
+}
